e8b2df4876c5c0428ecd64f26142fd9351240f83
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2025-01-22 16:25:21 +0000

    chore: refactore logs route

--- app/routes/logs.py
+++ app/routes/logs.py
@@ -1,24 +1,34 @@
 import json
-from fastapi import APIRouter,Depends
+import platform
+from fastapi import APIRouter,Depends, Query
 from sqlalchemy.testing import db
 from sqlalchemy.orm import Session
+from typing import List, Optional
 
 from app.core.database import get_db
 from app.crud.logs_crud import save_log
 from app.services.logs_service import  fetch_platform_logs
+import typing
 
 router=APIRouter()
 
 @router.get("/")
-async def fetch_and_save_logs(db: Session = Depends(get_db)):
-   logs= await fetch_platform_logs(["Application"])
-   print("startttt")
-   print(logs)
-   print("enddddd")
-   for log in logs[:10]:
-       # Serialize the log message if it's a dictionary
-        serialized_log = json.dumps(log)  # Assuming log is a dictionary
-        await save_log(db, log_source="windows", message=serialized_log, severity="INFO")
-   #
-   #
-   return {"status": "success", "logs_saved": len(logs)}
+async def fetch_and_save_logs(
+    log_types:Optional[typing.List[str]] = Query(None, description="List of log types to fetch")):
+    print(log_types)
+    try:
+        current_platform = platform.system()
+        if not log_types:
+            if current_platform == "Windows":
+                log_types = ["Application"]
+            elif current_platform == "Linux":
+                log_types = ["syslog"]
+            else:
+                return {"error": "Unsupported platform"}
+            
+        logs= await fetch_platform_logs(log_types)
+    
+        return {"status": "success", "logs_saved": logs}
+    except Exception as e:
+        return [f"Error fetching  logs: {str(e)}"]
+    
